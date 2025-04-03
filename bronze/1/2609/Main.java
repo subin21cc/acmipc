d78419cebd38f41cc6c61730bff02436f782fb23
subin <subin21cc@gmail.com>
2025-04-03 09:21:32 +0900

    add 2609

--- bronze/1/2609/Main.java
+++ bronze/1/2609/Main.java
@@ -3,26 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
 
-        int mo = 1;
-        int ja_1 = 1;
-        int ja_2 = 1;
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
 
-        for (int i = 1; i <= N; i++) {
-            mo *= i;
+        int max = 0, min = 0;
+
+        for (int i = 1; i <= n1 && i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                max = i;
+            }
         }
+        min = n1 * n2 / max;
 
-        for (int i = 1; i <= N - K; i++) {
-            ja_1 *= i;
-        }
-
-        for (int i = 1; i <= K; i++) {
-            ja_2 *= i;
-        }
-
-        System.out.println(mo / (ja_1 * ja_2));
-
+        System.out.println(max);
+        System.out.println(min);
     }
 }
